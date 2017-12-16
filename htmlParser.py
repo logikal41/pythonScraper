deccb8886550eb477197ac19bc874f3e8e94ca22
Matt Berry <mberry41@gmail.com>
2017-12-15 18:20:49 -0700

    start of document loop

--- htmlParser.py
+++ htmlParser.py
@@ -1,7 +1,7 @@
 from lxml import html
 from bs4 import BeautifulSoup
 
-with open("P:\QA\Berry\S18\shoe_section.html","r", encoding="utf8") as f:
+with open("P:\QA\Berry\S18\Section_2.html","r", encoding="utf8") as f:
 	soup = BeautifulSoup(f, 'lxml')
 
 #             ----- GLOBAL VARIABLES ------
@@ -14,123 +14,26 @@ spanClass = ['Section-Opener-Header', 'CharOverride-2', 'Category-Introduction',
 '_1_Product-Header', '_2_Product-Description', '_3_Style-Numbers']
 # pull all of the paragraphs in the soup
 paragraphs = soup.find_all('p')
-# Category Index
-categoryIndex = []
 
 
-#Initialize item dictionary (global variable)
-item = {'product category': '', 'product name': '', 'product description': '', 'features': '', \
-'m number': '', 'size': '', 'color': '', 'notes': ''}
-
-
-# Function to pull all the spans out of the targeted paragraphs
-def filterParagraphs(paragraphs, pClass):
-	# Pull in the global variable pClass
-	#global pClass
-	# local variables
-	spans = []
-
-	for p in paragraphs:
-		classes = p.attrs['class']
-		for item in classes:
-			if item in pClass:
-				nestedSpans = p.find_all('span')
-				for span in nestedSpans:
-					spans.append(span)
-	return spans
-
-# Function to pull the text out of the targeted spans
-def filterSpans(spans,spanClass):
-	# Pull in the global variable spanClass
-	#global spanClass
-	# local variables
-	items = []
-
-	for span in spans:
-		classes = span.attrs['class']
-		for item in classes:
-			if item in spanClass:
-				items.append(span.text)
-	return items
-
-
-# Function to pull out the climbing categories
-def climbingCategory(paragraphs):
-	# local variables for the class selectors
-	pClass = ['ParaOverride-4']
-	spanClass = ['Section-Opener-Header', 'CharOverride-2'] # if either of these classm es shows up we are good
-	spans = filterParagraphs(paragraphs,pClass)
-	return filterSpans(spans,spanClass)
-
-# Function to pull out the product names
-def productName(paragraphs):
-	# local variables for the class selectors
-	pClass = ['Product-Name']
-	spanClass = ['_1_Product-Header']
-	spans = filterParagraphs(paragraphs,pClass)
-	return filterSpans(spans,spanClass)
-
-# Function to pull out the product descriptions
-def productDescription(paragraphs):
-	# local variables for the class selectors
-	pClass = ['ParaOverride-3']
-	spanClass = ['_2_Product-Description']
-	spans = filterParagraphs(paragraphs,pClass)
-	return filterSpans(spans,spanClass)
-
-# Function to pull out the product features
-def productFeatures(paragraphs):
-	# local variables for the class selectors
-	pClass = ['Bullets', 'ParaOverride-3']  #this one needs a different function so that it includes both of these classes
-	spanClass = ['_2_Product-Description']
-	spans = filterParagraphs(paragraphs,pClass)
-	return filterSpans(spans,spanClass)
-
-
-
-# Find the category ( to many nested loop! break this into multiple functions to make the code easier to read) 
+# Find the category indexs 
 def findCategoryIndex(paragraphs):
-	global categoryIndex
-	# local variables for the class selectors
-	pClass = ['ParaOverride-4']
-	spanClass = ['Section-Opener-Header', 'CharOverride-2'] # if either of these classm es shows up we are good
+	index = [0]
 
-	# can this all go into its own function?
 	for p in range(len(paragraphs)):
-		classes = paragraphs[p].attrs['class'] # pull classes for each paragraph
-		for item in classes:
-			if item in pClass:   # see if the paragraph contains the targeted class
-				nestedSpans = paragraphs[p].find_all('span')  # pull all nested spans
-				for span in nestedSpans:
-					nestedClasses = span.attrs['class']  # get the classes of the nested spans
-					for spanItem in nestedClasses:
-						if spanItem in spanClass:  # see if the span contains the targeted class
-
-							#this part is unique to this function
-							if p not in categoryIndex: # see if this category has already been found
-								categoryIndex.append(p) # append the new category to the found categories
-								return p  # return the index of this category
-
-
+		if paragraphs[p][1] == "product_category":
+			if p not in index:
+				index.append(p) # append the new category to the found categories
+	index.append(len(paragraphs)) #this will be a stop point for loops
+	return index  # return the list of category indexes
 
 
 # Find the index at which to start populating data. First product is always before the categegory index.
 def findCategoryBeginning(paragraphs, categoryIndex):
-	# local variables for the class selectors
-	pClass = ['Product-Name']
-	spanClass = ['_1_Product-Header']
-
 	# This counts down instead of up
-	for p in range(categoryIndex,0,-1):  #start at the category index and go backwards to find the product name index
-		classes = paragraphs[p].attrs['class'] # pull the classes for each paragraph
-		for item in classes:
-			if item in pClass: # see if the paragraph contains the targeted class
-				nestedSpans = paragraphs[p].find_all('span') # pull all the nested spans
-				for span in nestedSpans:
-					nestedClasses = span.attrs['class']
-					for spanItem in nestedClasses:
-						if spanItem in spanClass: # see if the span contains the targeted class
-							return p # return the index of this product name
+	for p in range(categoryIndex,-1,-1):  #start at the category index and go backwards to find the product name index
+		if paragraphs[p][1] == "product_name":
+			return p # return the index of this product name
 
 
 # Functions to check Data Type
@@ -171,18 +74,16 @@ def isProductDescription(data):
 	classes = data.attrs['class']
 	for item in classes:
 		if item in pClass:
-			nestedSpans = data.find_all('span')
-			for span in nestedSpans:
-				nestedClasses = span.attrs['class']
-				for spanItem in nestedClasses:
-					if spanItem in spanClass:
-						return True
+			nestedSpan = data.find('span')
+			nestedClass = nestedSpan.attrs['class']
+			if nestedClass[0] in spanClass:
+				return True
 	return False
 
 
 def isProductFeature(data):
 	# local variables for the class selectors
-	pClass = ['Bullets', 'ParaOverride-3']  #this one needs a different function so that it includes both of these classes
+	pClass = ['Bullets', 'ParaOverride-3']  #must include both classes
 	spanClass = ['_2_Product-Description']
 	classes = data.attrs['class']
 	if classes == pClass:
@@ -202,12 +103,10 @@ def isProductNote(data):
 	classes = data.attrs['class']
 	for item in classes:
 		if item in pClass:
-			nestedSpans = data.find_all('span')
-			for span in nestedSpans:
-				nestedClasses = span.attrs['class']
-				for spanItem in nestedClasses:
-					if spanItem in spanClass:
-						return True
+			nestedSpan = data.find('span')
+			nestedClass = nestedSpan.attrs['class']
+			if nestedClass[0] in spanClass:
+				return True
 	return False
 
 def findDataType(data):
@@ -222,27 +121,78 @@ def findDataType(data):
 	elif isProductNote(data):
 		return 'product_note'	
 	else:
-		return ''
+		return 'na'
 
 def populateDataType(data):
 	item = [data.text]
 	item.append(findDataType(data))
 	return item
 
+def filterParagraphs(data):
+	if data[1] != 'na':
+		return True
+	return False
+
+def populateProduct(paragraphs,category,startIndex,endIndex):
+	#Initialize item dictionary
+	item = {'product_category': '', 'product_name': '', 'product_description': '', 'features': [], \
+	'm_number': '', 'size': '', 'color': '', 'notes': []}
+
+	item['product_category'] = category
+
+	for i in range(startIndex,endIndex,1):
+		if paragraphs[i][1] == "product_name":
+			item['product_name'] = paragraphs[i][0]
+		elif paragraphs[i][1] == "product_description":
+			item['product_description'] = paragraphs[i][0]
+		elif paragraphs[i][1] == "product_feature":
+			item['features'].append(paragraphs[i][0])
+		elif paragraphs[i][1] == "product_note":
+			item['notes'].append(paragraphs[i][0])
+	return item
+
+
+def documentLoop(paragraphs):
+	products = []
+	categoryIndex = findCategoryIndex(filteredParagraphs)
+
+	for i in range(len(categoryIndex)-2):
+		print("index of category = " + str(categoryIndex[i+1]))
+		firstProduct = findCategoryBeginning(paragraphs, categoryIndex[i+1])
+		# Need to continue looping each product name until the next product category
+		products.append(populateProduct(paragraphs,paragraphs[categoryIndex[i+1]][0],categoryIndex[i],categoryIndex[i+1]))
+	return products
+
+
+
+
+
+
+
+
+
+
+
+# ------- body of script -------
+
+
 sortedParagraphs = list(map(populateDataType, paragraphs))
+filteredParagraphs = list(filter(filterParagraphs, sortedParagraphs))
 
-for p in sortedParagraphs:
-	print(p)
+# Category Indexes
+categoryIndex = findCategoryIndex(filteredParagraphs)
 
 
+
+# product = populateProduct(filteredParagraphs,'shoes',0,11)
+# print(product)
+
+products = documentLoop(filteredParagraphs)
+for x in products:
+	print(x)
 
 
 # ------- TO DO --------
 # How to loop over this code so that each all individual parts can be linked to one dictionary (item)
 # Where is the start and end point for each loop so that all features get included, the m number, etc.
 # due to the layout of the HTML , the climbing category usually comes after the first item in the category
-
-# some product notes are showing up as product descriptions
-# EXAMPLE
-# ['Delivery date for Slate and Curry: 3/15/18', 'product_description']
-#  
